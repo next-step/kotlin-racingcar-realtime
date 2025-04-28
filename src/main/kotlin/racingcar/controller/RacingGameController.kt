de4be0744e69b6d7460f846c911547d6ee1bc70b
minhalee <minha1002@gmail.com>
2025-04-28 14:14:34 +0900

    feat(output): 컨트롤러에서 raceGame 클래스 분리

--- src/main/kotlin/racingcar/controller/RacingGameController.kt
+++ src/main/kotlin/racingcar/controller/RacingGameController.kt
@@ -1,50 +1,23 @@
 package racingcar.controller
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import racingcar.model.RaceGame
+import racingcar.model.RacingCar
 import racingcar.view.InputView
 import racingcar.view.OutputView
-import racingcar.model.RacingCar
-import racingcar.model.RaceGame
 
 object RacingGameController {
 
-    fun start() {
+    fun start() = runBlocking {
         val inputView = InputView()
         val outputView = OutputView()
 
-        // 사용자로부터 입력 받기
         val carNames = inputView.getCarNames()
         val goalDistance = inputView.getRoundCount()
 
         val cars = carNames.map { RacingCar(it) }
-        val raceGame = RaceGame(cars)
+        val raceGame = RaceGame(cars, goalDistance)
 
-        runBlocking {
-            runRace(raceGame, goalDistance, outputView)
-        }
+        raceGame.startRace(outputView)
     }
-
-    // 경주 실행 및 출력
-    private suspend fun runRace(raceGame: RaceGame, goalDistance: Int, outputView: OutputView) =
-        CoroutineScope(Dispatchers.Default).launch {
-            repeat(goalDistance) {
-                val jobs = launch {
-                    raceGame.runOneRound()
-                }
-
-                jobs.join()
-
-                val winners = raceGame.findWinners(goalDistance)
-                if (winners.isNotEmpty()) {
-                    jobs.cancel()
-
-                    // 우승자 출력
-                    outputView.announceWinners(winners)
-                }
-            }
-
-        }.join()
 }
