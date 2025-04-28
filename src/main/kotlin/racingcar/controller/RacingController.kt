a7f2ac4c6f49b2ae28fbdf25101777de7bf250ac
gullivernam <gullivernam94@gmail.com>
2025-04-28 16:01:31 +0900

    feat(Controller): pause/restart racing
    - enter 입력 시 racing 중지/재시작 구현
    
    Co-authored-by: ejlee0005 <ejlee0005@gmail.com>

--- src/main/kotlin/racingcar/controller/RacingController.kt
+++ src/main/kotlin/racingcar/controller/RacingController.kt
@@ -6,11 +6,14 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.NonCancellable.isActive
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import kotlinx.coroutines.yield
 import racingcar.model.Car
 import racingcar.view.RacingView
+import java.util.concurrent.atomic.AtomicBoolean
 
 class RacingController(
     private val racingView: RacingView,
@@ -20,6 +23,7 @@ class RacingController(
     var goal: Int = 0
 
     private val scope = CoroutineScope(dispatcher + SupervisorJob())
+    private val isPaused = AtomicBoolean(false)
 
     fun initRacing() {
         cars = racingView.nameInputView().map { Car(it) }
@@ -30,17 +34,37 @@ class RacingController(
         cars
             .map {
                 scope.launch { move(it) }
+                scope.launch { waitCommand() }
             }.joinAll()
+    }
+
+    private suspend fun waitCommand() {
+        withContext(Dispatchers.IO) {
+            while (isActive) {
+                if (System.`in`.available() > 0) {
+                    val input = readln()
+                    if (input.isEmpty()) { // 엔터 키 입력
+                        isPaused.set(!isPaused.get())
+                        println("코루틴 ${if (isPaused.get()) "일시중지" else "재개"}")
+                    }
+                }
+                delay(1) // 입력 체크 간격
+            }
+        }
     }
 
     private suspend fun move(car: Car) {
         while (isActive && car.position < goal) {
-            yield() // while 이 종료되는 시점이 car.forward 내부의 delay 에 걸리기 전에 종료
-            car.forward()
-            racingView.positionView(car)
-            if (car.position == goal) {
-                racingView.resultView(car)
-                scope.cancel()
+            if (!isPaused.get()) {
+                yield() // while 이 종료되는 시점이 car.forward 내부의 delay 에 걸리기 전에 종료
+                car.forward()
+                racingView.positionView(car)
+                if (car.position == goal) {
+                    racingView.resultView(car)
+                    scope.cancel()
+                }
+            } else {
+                delay(500)
             }
         }
     }
