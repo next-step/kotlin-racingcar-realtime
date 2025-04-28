35ddbdf524885d0762f1d3396a3178f882d51bc9
ase3238 <ase3238@gmail.com>
2025-04-28 14:13:03 +0900

    test: runRound Test code 구현

---
+++ src/test/kotlin/controller/RaceControllerTest.kt
@@ -0,0 +1,37 @@
+package controller
+
+import entity.Car
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.runTest
+import model.RaceModel
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import view.RaceView
+
+class RaceControllerTest {
+    @Test
+    fun runRoundTest() = runTest {
+        val cars: List<Car> = listOf(
+            Car("car1", 0),
+            Car("car2", 0),
+            Car("car3", 9),
+        )
+        val goal = 10
+        val model = RaceModel()
+        val view = RaceView()
+        val controller = RaceController(model, view)
+
+        controller.runRound(
+            cars,
+            goal,
+            CoroutineScope(Dispatchers.Unconfined)
+        )
+
+        cars.firstOrNull { it.distance == goal }?.name shouldBe "car3"
+    }
+
+}
