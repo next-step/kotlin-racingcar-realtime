ddcae0aa3e1e3ded0eb23f500b2e78c4b0e78d6d
ase3238 <ase3238@gmail.com>
2025-04-28 10:55:48 +0900

    refac: model 내 변수 controller로 migration

--- src/main/kotlin/controller/RaceController.kt
+++ src/main/kotlin/controller/RaceController.kt
@@ -1,5 +1,6 @@
 package controller
 
+import entity.Car
 import model.RaceModel
 import view.RaceView
 
@@ -8,44 +9,46 @@ class RaceController(
     val raceView: RaceView,
 ) {
     fun runGame() {
-        initCarList()
-        initRound()
-        runRound()
-        runAward()
+        val carList = initCarList()
+        val goal = initGoal()
+        runRound(carList, goal)
+        runAward(carList)
     }
 
-    private fun initCarList() {
-        while (raceModel.carList.isEmpty()) {
+    private fun initCarList(): List<Car> {
+        while (true) {
             try {
                 raceView.showCarInitMsg()
-                raceModel.initCarList(readln())
+                return raceModel.initCarList(readln())
             } catch (e: IllegalArgumentException) {
                 handleError(e)
             }
         }
     }
 
-    private fun initRound() {
-        while (raceModel.round < 1) {
+    private fun initGoal(): Int {
+        while (true) {
             try {
                 raceView.showRoundInitMsg()
-                raceModel.initRound(readln())
+                return raceModel.initGoal(readln())
             } catch (e: IllegalArgumentException) {
                 handleError(e)
             }
         }
     }
 
-    private fun runRound() {
+    private fun runRound(carList: List<Car>, goal: Int) {
         raceView.showRoundResult()
-        repeat(raceModel.round) {
-            raceModel.runRound()
-            raceView.showEachRoundResult(raceModel.carList)
+        repeat(goal) {
+            carList.forEach {
+                raceModel.runRound(it)
+            }
+            raceView.showEachRoundResult(carList)
         }
     }
 
-    private fun runAward() {
-        val winners = raceModel.getWinners()
+    private fun runAward(carList: List<Car>) {
+        val winners = raceModel.getWinners(carList)
         raceView.showWinners(winners)
     }
 
--- src/main/kotlin/model/RaceModel.kt
+++ src/main/kotlin/model/RaceModel.kt
@@ -3,41 +3,37 @@ package model
 import entity.Car
 
 class RaceModel {
-    var carList: List<Car> = emptyList()
-    var round: Int = 0
-
-    fun initCarList(input: String) {
+    fun initCarList(input: String): List<Car> {
         if (input.isEmpty()) throw IllegalArgumentException("입력된 이름이 없습니다.")
-        carList =
-            input.split(",").map {
-                if (it.isEmpty()) throw IllegalArgumentException("입력된 이름이 없습니다.")
-                if (it.length > 5) throw IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.")
-                Car(it, 0)
-            }
+        return input.split(",").map {
+            if (it.isEmpty()) throw IllegalArgumentException("입력된 이름이 없습니다.")
+            if (it.length > 5) throw IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.")
+            Car(it, 0)
+        }
     }
 
-    fun initRound(input: String) {
+    fun initGoal(input: String): Int {
         if (input.isEmpty()) throw IllegalArgumentException("입력된 횟수가 없습니다.")
         try {
-            round = input.toInt()
-            if (round < 1) {
+            val goal = input.toInt()
+            if (goal < 1) {
                 throw IllegalArgumentException("1 이상의 숫자만 입력 가능합니다.")
             }
+            return goal
         } catch (_: NumberFormatException) {
             throw IllegalArgumentException("횟수는 숫자만 입력 가능합니다.")
         }
     }
 
-    fun runRound() {
-        carList.forEach {
-            val random = (0..9).random()
-            if (random >= 4) {
-                it.move()
-            }
+    fun runRound(car: Car): Car {
+        val random = (0..9).random()
+        if (random >= 4) {
+            car.move()
         }
+        return car
     }
 
-    fun getWinners(): List<String> {
+    fun getWinners(carList: List<Car>): List<String> {
         val maxDist = carList.maxOfOrNull { it.distance } ?: throw IllegalStateException("자동차가 없습니다.")
         return carList.filter { it.distance == maxDist }.map { it.name }
     }
