87bc6f53f2d5a5ecf689e635eb8aa8d1b29f708d
Taewoong Kim <twkim8912@gmail.com>
2025-04-29 09:36:19 +0900

    feat(data): Car 클래스 추가

---
+++ src/main/kotlin/model/Car.kt
@@ -0,0 +1,18 @@
+package model
+
+import kotlinx.coroutines.delay
+import kotlin.ranges.random
+import kotlin.text.repeat
+import kotlin.time.Duration.Companion.milliseconds
+
+data class Car(
+    val carName: String,
+    var position: Int = 0,
+) {
+    suspend fun move() {
+        val duration = (0..500).random().milliseconds
+        delay(duration)
+        position++
+        println("$carName : ${"-".repeat(position)}")
+    }
+}
